045def2318d525d01fbedb71a4fb0e7ae8c73dc9
cuong <c.pham@odysseemobile.com>
2022-01-18 15:50:23 +0700

    ASG-1903 Odoo Marketplace: Presentation (init)

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Odyssee Field Service Odoo Connector',
+    'version':'1.0',
+    'sequence': 1,
+    'description':'Odyssee Field Service Odoo Connector',
+    'author':'Odyssee Mobile',
+    'website':'https://odysseemobile.com/',
+    'license':'LGPL-3',
+    'installable':'False',
+    'images':['static/description/ods_description.gif'],
+    'live_test_url': 'https://portal.odysseemobile.com/marketplace?connect=odoo',
+}
